ec7832c4c894825f5fd23978dd6255fa5397a57b
GoldFrosch <goldfrosch@daum.net>
2024-08-24 10:12:12 +0900

    feat: backend domain 적용

--- example-2/vite.config.ts
+++ example-2/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target:
+          "http://ec2-43-203-233-67.ap-northeast-2.compute.amazonaws.com:8080/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/api/, ""),
         secure: false,
